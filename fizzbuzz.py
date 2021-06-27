8311b6840585fee6859c293c79b17f1d94bfb945
kenjikhk <85302843+kenjikhk@users.noreply.github.com>
2021-06-27 09:52:35 +0800

    Add files via upload

---
+++ fizzbuzz.py
@@ -0,0 +1,12 @@
+for fizzbuzz in range(1,1000):
+    if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
+        print("fizzbuzz")
+        
+    elif fizzbuzz % 3 == 0:
+        print("fizz")
+        
+    elif fizzbuzz % 5 == 0:
+        print("buzz")
+    else:
+        print(fizzbuzz)
+
